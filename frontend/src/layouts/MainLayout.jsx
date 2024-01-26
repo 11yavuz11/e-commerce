4e450f61064e39f5dcf91a8cc6b6caa3a1a5e068
Veli Yavuz <yavuz.veli.11@gmail.com>
2024-01-26 03:30:02 +0300

    Search modal added

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import { useState } from "react";
 import Footer from "../components/Layout/Footer/Footer";
 import Header from "../components/Layout/Header/Header";
 import PropTypes from "prop-types";
+import Search from "../components/Modals/Search/Search";
 
 const MainLayout = ({ children }) => {
+  const [isSearchShow, setIsSearchShow] = useState(false);
   return (
-    <React.Fragment>
-      <Header />
+    <div className="main-layout">
+      <Search isSearchShow={isSearchShow} setIsSearchShow={setIsSearchShow} />
+      <Header setIsSearchShow={setIsSearchShow} />
       {children}
       <Footer />
-    </React.Fragment>
+    </div>
   );
 };
 
