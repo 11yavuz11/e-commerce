ec0dc31dc6bf6624b8eb4ffb78348b8caa81e8cf
Veli Yavuz <yavuz.veli.11@gmail.com>
2024-02-06 04:18:19 +0300

    User Api Endpoint Finished

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,11 +2,16 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.js");
 const bcrypt = require("bcryptjs");
+const generateRandomAvatar = () => {
+  const randomAvatar = Math.floor(Math.random() * 71);
+  return `https://i.pravatar.cc/300?image=${randomAvatar}`;
+};
 
 // Register
 router.post("/register", async (req, res) => {
   try {
     const { username, email, password } = req.body;
+    const defautAvatar = generateRandomAvatar();
 
     const existingUser = await User.findOne({ email });
     const existingUsername = await User.findOne({ username });
@@ -22,6 +27,7 @@ router.post("/register", async (req, res) => {
       username,
       email,
       password: hashedPassword,
+      avatar: defautAvatar,
     });
     await newUser.save();
 
@@ -30,6 +36,34 @@ router.post("/register", async (req, res) => {
       .json({ message: `User created successfully. User: ${newUser}` });
 
     console.log(req.body);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: `Server Error: ${error}` });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+    const isPassswordVaid = await bcrypt.compare(password, user.password);
+    if (!isPassswordVaid) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    res.status(200).json({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      avatar: user.avatar,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: `Server Error: ${error}` });
